013cc2e276340864f341fd80b22c49f3edebb4e7
YulunGuo <guoyulun@shu.edu.cn>
2022-12-08 22:55:17 +0800

    修改Matlab内容

--- shu_project/Matlib与Simulink学习/Untitled.m
+++ shu_project/Matlib与Simulink学习/Untitled.m
@@ -1,16 +1,17 @@
 clear
 clc
 
-M = 20;
-N = 12;
-K = M + N + 1;
+t = linspace(-2*pi,2*pi);
 
-w = -pi : pi/2000 :pi;
+h_t = sqrt(2)*(sin(t) + cos(t));
 
-H_jw = (1/K)*exp(1i*w*((N-M)/2)).*sin(w*(K/2))./sin(w/2);
 
-plot(w,abs(H_jw));
+plot(t,h_t);
+hold on;
+axis on;
+plot(t,fft(h_t))
 xlabel("w");
 ylabel("H(e^{jw})");
-xlim([-pi pi]);
+xlim([-2*pi,2*pi]);
+legend("h(t)","FFT(h(t))");
 
